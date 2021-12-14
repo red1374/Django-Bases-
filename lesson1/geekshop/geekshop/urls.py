ae120d4a2517e762e989c42340c762993fa53952
Плешаков Сергей <main@pleshakov.org>
2021-12-15 00:28:03 +0500

    INIT Lesson 2

--- lesson1/geekshop/geekshop/urls.py
+++ lesson1/geekshop/geekshop/urls.py
@@ -20,7 +20,7 @@ from .views import index, contacts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('contacts/', contacts),
+    path('', index, name='main'),
+    path('contacts/', contacts, name='contacts'),
     path('products/', include('products.urls'))
 ]
--- lesson1/geekshop/geekshop/views.py
+++ lesson1/geekshop/geekshop/views.py
@@ -1,9 +1,26 @@
+import json
+
 from django.shortcuts import render
 
 
+def getTopMenu():
+    with open('menu_items.json') as data_file:
+        data = json.load(data_file)
+
+    return data
+
+
 def index(request):
-    return render(request, 'index.html')
+    content = {
+        'title': 'главная',
+        'menu_items': getTopMenu()
+    }
+    return render(request, 'index.html', context=content)
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    content = {
+        'title': 'контакты',
+        'menu_items': getTopMenu()
+    }
+    return render(request, 'contacts.html', context=content)
